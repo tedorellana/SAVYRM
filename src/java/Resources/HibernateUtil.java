908b1b85200fdee1e70e993265683ba8233be332
tedorellana <tedorellana@gmail.com>
2018-05-25 23:35:03 -0500

    cambios locales no oficiales

--- src/java/Resources/HibernateUtil.java
+++ src/java/Resources/HibernateUtil.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package Resources;
 
 import org.hibernate.cfg.AnnotationConfiguration;
@@ -12,7 +13,7 @@ import org.hibernate.SessionFactory;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author torellana
+ * @author TED
  */
 public class HibernateUtil {
 
